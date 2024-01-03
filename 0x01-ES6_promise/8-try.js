b37e63686be239d22aaea6631f68f3e16ebe790f
oberon10 <prosperbernrad@gmail.com>
2024-01-03 22:29:05 +0000

    8-try.js

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+export default function divideFunction(number, denominator) {
+  if (denominator === 0) {
+    throw new Error("connot divide by 0");
+  }
+  return numerator / denominator;
+}
