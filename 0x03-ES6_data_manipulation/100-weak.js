f9458e321dd752d0713e853fee5652948b56fc49
oberon10 <prosperbernrad@gmail.com>
2024-01-09 19:23:32 +0000

    100-weak.js

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,11 @@
+export const weakMap = new WeakMap();
+let count = 1;
+
+export function queryAPI(endPoint) {
+  WeakMap.set(endPoint, count);
+  count += 1;
+  const queryCount = WeakMap.get(endPoint);
+  if (queryCount >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+}
