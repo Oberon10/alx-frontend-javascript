20d33ab4231810f19500f528fde8c7152424ea05
oberon10 <prosperbernrad@gmail.com>
2024-01-10 16:48:45 +0000

    get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,10 @@
-export default function getListStudents() {
+function getListStudents() {
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
 }
+
+// Exporting the function to make it accessible in other files.
+module.exports = getListStudents;
